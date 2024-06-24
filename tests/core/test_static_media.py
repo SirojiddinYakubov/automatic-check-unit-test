b8e8763c5eb4631a60ad4a989c4b6479f7fba57a
Abdulaziz Komilov <abdulazizkomilov2001@gmail.com>
2024-06-24 17:26:42 +0500

    Created Unit tests for static, media files and CustomUser avatar

---
+++ tests/core/test_static_media.py
@@ -0,0 +1,10 @@
+import pytest
+from django.conf import settings
+
+
+@pytest.mark.django_db
+def test_static_media_settings():
+    assert settings.STATIC_URL == '/static/', "STATIC_URL is not set correctly"
+    assert settings.STATIC_ROOT == settings.BASE_DIR / "staticfiles", "STATIC_ROOT is not set correctly"
+    assert settings.MEDIA_URL == '/media/', "MEDIA_URL is not set correctly"
+    assert settings.MEDIA_ROOT == settings.BASE_DIR / "media", "MEDIA_ROOT is not set correctly"

398245f85b6c92ab7195b978d4feb878fcb7efc3
Sirojiddin Yakubov <yakubov9791999@gmail.com>
2024-07-11 12:44:15 +0500

    fix bug

--- tests/proyekt-maxfiy-malumotlarini-environmentda-saqlash/test_is_installed_decouple.py
+++ tests/proyekt-maxfiy-malumotlarini-environmentda-saqlash/test_is_installed_decouple.py
@@ -1,6 +1,6 @@
 import importlib.util
 
 
-def test_is_installed_decouple():
+def test_via_importlib():
     loader = importlib.util.find_spec('decouple')
     assert loader is not None, "decouple is not installed"
